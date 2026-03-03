7f919df4b4447e72127e7e74504cfcd0ef9edbb0
cristianjosueperezecheverry-ai <cristianjosueperezecheverry@gmail.com>
2026-03-03 08:08:34 -0500

    feat: resuelto ejercicio 01 - conversor de temperatura

---
+++ ejercicio_1/ejercicio_1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <iomanip> // Para setprecision
+
+using namespace std;
+
+int main() {
+    double celsius;
+    cout << "Ingrese grados Celsius: ";
+    cin >> celsius;
+
+    double fahrenheit = (celsius * 9/5) + 32;
+    double kelvin = celsius + 273.15;
+    double rankine = (celsius * 9/5) + 491.67;
+
+    cout << fixed << setprecision(2);
+    cout << "Fahrenheit: " << fahrenheit << endl;
+    cout << "Kelvin: " << kelvin << endl;
+    cout << "Rankine: " << rankine << endl;
+    return 0;
+}
